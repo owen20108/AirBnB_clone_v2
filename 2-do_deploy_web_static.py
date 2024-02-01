62899f2687408fd6a0baa10fd02b3de95119cf04
owen20108 <owen20108@yahoo.com>
2024-02-01 14:07:13 -0800

    iarbnb

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from os import path
 
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['34.201.174.70', '100.25.35.110']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
