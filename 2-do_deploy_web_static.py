9f48b96fd6971ac60939e6ebf3c690a44ee02608
owen20108 <owen20108@yahoo.com>
2024-02-17 15:09:30 -0800

    air

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,34 +1,56 @@
 #!/usr/bin/python3
-# Fabric script that generates a .tgz archive from the
-# contents of the web_static folder of your AirBnB Clone repo
-# using the function do_pack
-import os
-from fabric.api import run, put, env
+"""Compress web static package
+"""
+from fabric.api import *
+from datetime import datetime
+from os import path
+
 
 env.hosts = ['34.201.174.70', '100.25.35.110']
-env.user = "ubuntu"
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_deploy(archive_path):
-    """Create a tar gzipped archive of the directory web_static."""
-    if os.path.exists(archive_path) is False:
-        return False
-    else:
+        """Deploy web files to server
+        """
         try:
-            put(archive_path, "/tmp/")
-            file_name = archive_path.split("/")[1]
-            file_name2 = file_name.split(".")[0]
-            final_name = "/data/web_static/releases/" + file_name2 + "/"
-            run("mkdir -p " + final_name)
-            run("tar -xzf /tmp/" + file_name + " -C " + final_name)
-            run("rm /tmp/" + file_name)
-            run("mv " + final_name + "web_static/* " + final_name)
-            run("rm -rf " + final_name + "web_static")
-            run("rm -rf /data/web_static/current")
-            run("ln -s " + final_name + " /data/web_static/current")
-            print("New version deployed!")
-            return True
-        except Exception as e:
-            print("Error:", e)
-            return False
+                if not (path.exists(archive_path)):
+                        return False
 
+                # upload archive
+                put(archive_path, '/tmp/')
+
+                # create target dir
+                timestamp = archive_path[-18:-4]
+                run('sudo mkdir -p /data/web_static/\
+releases/web_static_{}/'.format(timestamp))
+
+                # uncompress archive and delete .tgz
+                run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
+/data/web_static/releases/web_static_{}/'
+                    .format(timestamp, timestamp))
+
+                # remove archive
+                run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
+
+                # move contents into host web_static
+                run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
+/data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
+
+                # remove extraneous web_static dir
+                run('sudo rm -rf /data/web_static/releases/\
+web_static_{}/web_static'
+                    .format(timestamp))
+
+                # delete pre-existing sym link
+                run('sudo rm -rf /data/web_static/current')
+
+                # re-establish symbolic link
+                run('sudo ln -s /data/web_static/releases/\
+web_static_{}/ /data/web_static/current'.format(timestamp))
+        except:
+                return False
+
+        # return True on success
+        return True
--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,31 +1,27 @@
 #!/usr/bin/python3
-"""Start web application with two routings
-"""
-
+""" Starts a Flash Web Application """
 from models import storage
 from models.state import State
 from flask import Flask, render_template
 app = Flask(__name__)
-
-
-@app.route('/states_list')
-def states_list():
-    """Render template with states
-    """
-    path = '7-states_list.html'
-    states = storage.all(State)
-    # sort State object alphabetically by name
-    sorted_states = sorted(states.values(), key=lambda state: state.name)
-    return render_template(path, sorted_states=sorted_states)
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
 
 
 @app.teardown_appcontext
-def app_teardown(arg=None):
-    """Clean-up session
-    """
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
     storage.close()
 
 
-if __name__ == '__main__':
-    app.url_map.strict_slashes = False
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ displays a HTML page with a list of states """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
+    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
